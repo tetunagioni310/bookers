f24fbe122c75d48d0e591acf202507430b0d6b3b
tetunagioni310 <104610603+tetunagioni310@users.noreply.github.com>
2022-09-01 12:28:30 +0000

    [finish]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "successfully"
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "削除しました"
+    flash[:notice] = "Book was successfully destroy."
     redirect_to "/books"
   end
 
@@ -34,7 +34,7 @@ class BooksController < ApplicationController
   def update
    @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "successfully"
+      flash[:notice] = "Book was successfully update."
       redirect_to book_path(@book.id)
     else
       render 'edit'
